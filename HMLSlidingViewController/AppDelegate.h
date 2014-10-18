ee1c7167021d4a60c7ed630e87e44b2e200e9659
慕楼 <mulou.zzy@taobao.com>
2014-10-18 23:46:44 +0800

    init

---
+++ HMLSlidingViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HMLSlidingViewController
+//
+//  Created by huamulou on 14-10-18.
+//  Copyright (c) 2014年 showmethemoney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HMLSlidingViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HMLSlidingViewController
+//
+//  Created by huamulou on 14-10-18.
+//  Copyright (c) 2014年 showmethemoney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
